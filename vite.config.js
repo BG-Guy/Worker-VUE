56fadc7e6884ab21da1983b4b95a8a95ed3ee220
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2023-03-07 02:30:19 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'url'
-import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "url";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),VitePWA({})],
+  plugins: [vue(), VitePWA({})],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  build:{
-    outDir:'../backend/public',
-    emptyOutDir: true
-  }
-})
+  build: {
+    outDir: "./public",
+    emptyOutDir: true,
+  },
+});
